3601da19709fad168881d58cc6e8b20c5c0590fe
rsy888 <rohsy0818@khu.ac.kr>
2021-05-25 21:24:22 +0900

    update

--- backend/urls.py
+++ backend/urls.py
@@ -4,11 +4,15 @@ from backend import views
 
 urlpatterns = [
     url(r'^sign_up/$', views.SignUp.as_view()),
+    url(r'^sign_out/$',views.SignOut.as_view()),
     url(r'^confirm_sign_up/$',views.ConfirmSignUp.as_view()),
     url(r'^confirm_email/$',views.ConfirmEmail.as_view()),
     url(r'^get_email_verification/$',views.GetEmailVerification.as_view()),
     url(r'^admin_initiate_user/$', views.AdminInitiateAuth.as_view()),
-    url(r'^public_provider_login/$', views.PublicProviderLogin.as_view()),
+    #url(r'^public_provider_login/$', views.PublicProviderLogin.as_view()),
     url(r'^change_password/$', views.ChangePassword.as_view()),
-    url(r'^retrieve_info/$', views.RetrieveInfo.as_view())
+    url(r'^forgot_password/$', views.ForgotPassword.as_view()),
+    url(r'^confirm_forgot_password/$',views.ConfirmForgotPassword().as_view()),
+    url(r'^retrieve_info/$', views.RetrieveInfo.as_view()),
+    url(r'^delete_user/$', views.DeleteUser.as_view())
 ]
--- backend/views.py
+++ backend/views.py
@@ -33,21 +33,32 @@ class SignUp(APIView):
             #return HttpResponse("비밀번호는 최소 6자리입니다.")
         return Response(data={'user':user}, status=status.HTTP_201_CREATED)
 
-# confirm 회원가입
+# 회원가입 확인
 class ConfirmSignUp(APIView):
     def post(self, request, *ars, **kwargs):
         try:
             idp_client = boto3.client('cognito-idp', **settings.DEFAULT_CONFIG)
+            username=''
+            if(settings.USERNAME==''):
+                username=request.data['username']
+            else:
+                username=settings.USERNAME
 
-            user = idp_client.admin_confirm_sign_up(UserPoolId=settings.DEFAULT_USER_POOL_ID,
-                                    Username=request.data['username']
+            user = idp_client.confirm_sign_up(ClientId=settings.DEFAULT_USER_POOL_APP_ID,
+                                    Username=username,
+                                    ConfirmationCode=request.data['code']
                                     )
 
             return Response(data={'user':user}, status=status.HTTP_201_CREATED)
 
-        except idp_client.exceptions.UsernameExistsException:
-            return HttpResponse("이미 존재하는 id입니다.")
+        except botocore.exceptions.ParamValidationError:
+            return HttpResponse("다시 시도해주세요")
+        except idp_client.exceptions.ExpiredCodeException:
+            return HttpResponse("유효하지 않은 코드입니다.")
+        except idp_client.exceptions.CodeMismatchException:
+            return HttpResponse("올바르지 않은 코드입니다.")
 
+'''
 # 이메일 확인
 class ConfirmEmail(APIView):
     def post(self, request, *ars, **kwargs):
@@ -67,28 +78,35 @@ class ConfirmEmail(APIView):
             return HttpResponse("유효하지 않은 코드입니다.")
         except idp_client.exceptions.CodeMismatchException:
             return HttpResponse("올바르지 않은 코드입니다.")
+'''
 
-# 이메일 확인 코드 재발급
+# 회원가입 확인 코드 재발급
 class GetEmailVerification(APIView):
     def get(self, request, *ars, **kwargs):
         try:
             idp_client = boto3.client('cognito-idp', **settings.DEFAULT_CONFIG)
 
-            user = idp_client.get_user_attribute_verification_code(AccessToken=settings.ACCESS_TOKEN,
-                                    AttributeName='email')
+            user = idp_client.confirm_sign_up(ClientId=settings.DEFAULT_USER_POOL_APP_ID,
+                                    Username=username
+                                    )
 
-            return Response(data={'user':user}, status=status.HTTP_201_CREATED)
-        
-        except idp_client.exceptions.InvalidParameterException:
-            return HttpResponse("로그인이 필요합니다.")
-        except idp_client.exceptions.ExpiredCodeException:
-            return HttpResponse("유효하지 않은 코드입니다.")
+            return HttpResponse('%s로 이메일을 보냈습니다'%user['CodeDeliveryDetails']['Destination'])
+        except idp_client.exceptions.NotAuthorizedException:
+            return HttpResponse("asdf")        
 
-'''
+
+# 로그아웃
 class SignOut(APIView):
     def get(self,request, *args, **kwargs):
-        idp_client = boto3.client('cognito-idp', **settings.DEFAULT_CONFIG)
-'''
+        try:
+            idp_client = boto3.client('cognito-idp', **settings.DEFAULT_CONFIG)
+
+            user=idp_client.global_sign_out(AccessToken=settings.ACCESS_TOKEN)
+
+            return HttpResponse("로그아웃 되었습니다.")
+        except idp_client.exceptions.NotAuthorizedException:
+            return HttpResponse("로그아웃 상태입니다.")
+
         
 # 로그인
 class AdminInitiateAuth(APIView):
@@ -117,7 +135,7 @@ class AdminInitiateAuth(APIView):
         except idp_client.exceptions.NotAuthorizedException:
             return HttpResponse("아이디 또는 비밀번호가 일치하지 않습니다.")
         
-    
+'''    
 class PublicProviderLogin(APIView):
     def post(self, request, *args, **kwargs):
         token = request.data['token']
@@ -133,6 +151,7 @@ class PublicProviderLogin(APIView):
                                         }
                                 )
         return Response(data={'res':res}, status=status.HTTP_201_CREATED)
+'''
 
 # 비밀번호 변경        
 class ChangePassword(APIView):
@@ -156,6 +175,43 @@ class ChangePassword(APIView):
         except idp_client.exceptions.InvalidParameterException:
             return HttpResponse("로그인이 필요합니다.")
 
+
+# 비밀번호 잊어버렸을 때        
+class ForgotPassword(APIView):
+    def post(self,request,*args,**kwargs):
+        try:
+            idp_client = boto3.client('cognito-idp', **settings.DEFAULT_CONFIG)
+            user=idp_client.forgot_password(ClientId=settings.DEFAULT_USER_POOL_APP_ID,
+                                    Username=request.data['username']
+                                    )
+
+            settings.USERNAME=request.data['username']
+            
+            return HttpResponse('%s로 이메일을 보냈습니다'%user['CodeDeliveryDetails']['Destination'])
+        except idp_client.exceptions.NotAuthorizedException:
+            return HttpResponse("asdf")
+
+
+# 비밀번호 잊어버린 거 Confirm       
+class ConfirmForgotPassword(APIView):
+    def post(self,request,*args,**kwargs):
+        try:
+            idp_client = boto3.client('cognito-idp', **settings.DEFAULT_CONFIG)
+            user=idp_client.confirm_forgot_password(ClientId=settings.DEFAULT_USER_POOL_APP_ID,
+                                    Username=settings.USERNAME,
+                                    Password=request.data['newpassword'],
+                                    ConfirmationCode=request.data['code']
+                                    )
+            
+            return HttpResponse("비밀번호가 변경되었습니다.")
+        except botocore.exceptions.ParamValidationError:
+            return HttpResponse("다시 시도해주세요")
+        except idp_client.exceptions.ExpiredCodeException:
+            return HttpResponse("유효하지 않은 코드입니다.")
+        except idp_client.exceptions.CodeMismatchException:
+            return HttpResponse("올바르지 않은 코드입니다.")
+
+
 # 사용자 정보 조회
 class RetrieveInfo(APIView):
     def get(self,request,*args,**kwargs):
@@ -168,5 +224,17 @@ class RetrieveInfo(APIView):
         except:
             return HttpResponse("로그인이 필요합니다.")
 
+
+# 사용자 삭제 탈퇴
+class DeleteUser(APIView):
+    def get(self,request,*args,**kwargs):
+        try:
+             idp_client = boto3.client('cognito-idp', **settings.DEFAULT_CONFIG)
+             user=idp_client.delete_user(AccessToken=settings.ACCESS_TOKEN)
+
+             return HttpResponse("탈퇴 되었습니다.")
+
+        except:
+            return HttpResponse("로그인이 필요합니다.")
 
             
